6182aec810e8463e222dd008e9463be10b6a1d94
dlimachi <desiree.limachi@gmail.com>
2022-09-03 13:01:02 -0300

    modify db for users and news

--- persistence/src/test/resources/sql/schema.sql
+++ persistence/src/test/resources/sql/schema.sql
@@ -1,5 +1,21 @@
 CREATE TABLE IF NOT EXISTS users (
     userId SERIAL PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL
+    email          VARCHAR(100)   UNIQUE NOT NULL,
+    username       VARCHAR(50)    UNIQUE ,
+    pass           VARCHAR(200)   ,
+    status         TEXT           NOT NULL,
+    image_id       INTEGER        ,
+    FOREIGN KEY (image_id) REFERENCES image (image_id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS news (
+    news_id           SERIAL          PRIMARY KEY,
+    body              TEXT            NOT NULL,
+    title             VARCHAR(200)    NOT NULL,
+    subtitle          VARCHAR(200)    NOT NULL,
+    creator           INTEGER         NOT NULL,
+    creation_date     TIMESTAMP       NOT NULL,
+    image_id          INTEGER         ,
+
+    FOREIGN KEY (creator) REFERENCES users(user_id) ON DELETE SET NULL
 );
