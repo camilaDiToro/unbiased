445daa84106aaee3c71b98644de1279d5b94b77e
khcatino <kcatino@itba.edu.ar>
2022-09-20 08:55:17 -0300

    Fix top creators query

--- persistence/src/main/java/ar/edu/itba/paw/persistence/UserJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserJdbcDao.java
@@ -92,7 +92,11 @@ public class UserJdbcDao implements UserDao {
     @Override
     public List<User> getTopCreators(int qty) {
         return jdbcTemplate.query("WITH interactions AS (SELECT creator AS user_id, count(*) AS interaction_count FROM upvotes JOIN news ON upvotes.news_id = news.news_id " +
-                "WHERE DATE(interaction_date) = CURRENT_DATE GROUP BY creator ORDER BY count(*) DESC LIMIT ?) SELECT * FROM interactions NATURAL JOIN users", new Object[]{qty},ROW_MAPPER);
+                "WHERE DATE(interaction_date) = CURRENT_DATE GROUP BY creator LIMIT ?) SELECT * FROM interactions NATURAL JOIN users ORDER BY interaction_count DESC", new Object[]{qty},ROW_MAPPER);
 
     }
 }
+
+/*
+SELECT creator AS user_id, count(*) AS interaction_count FROM upvotes JOIN news ON upvotes.news_id = news.news_id WHERE DATE(interaction_date) = CURRENT_DATE GROUP BY creator ORDER BY count(*) DESC LIMIT 5;
+ */
