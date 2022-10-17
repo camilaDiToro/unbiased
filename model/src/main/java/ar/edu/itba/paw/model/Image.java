1bfae607b33b764f4e2627cb2966164da08c8e7a
camilaDiToro <camiladitoro@gmail.com>
2022-10-17 20:25:13 -0300

    Image as entity

--- model/src/main/java/ar/edu/itba/paw/model/Image.java
+++ model/src/main/java/ar/edu/itba/paw/model/Image.java
@@ -1,9 +1,21 @@
 package ar.edu.itba.paw.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "image")
 public class Image {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "image_image_id_seq")
+    @SequenceGenerator(name="image_image_id_seq", sequenceName = "image_image_id_seq", allocationSize = 1)
+    @Column(name = "image_id")
     private final long imageId;
+
+    @Column(name = "bytes")
     private final byte[] bytes;
+
+    @Column(name = "data_type")
     private final String dataType;
 
     public Image(long imageId, byte[] bytes, String dataType) {
