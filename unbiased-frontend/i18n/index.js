393ea889246f8fe96257e60cbfc66979db167aa6
dlimachi <dlimachi@itba.edu.ar>
2023-01-02 23:01:39 -0300

    Add internationalization in spanish for index view

--- unbiased-frontend/i18n/index.js
+++ unbiased-frontend/i18n/index.js
@@ -2,14 +2,15 @@ import { useRouter } from "next/router";
 
 const strings = {
   "en-US": {
-    "home.createArticle.button": "Crear",
-    "views.register.title": "Register",
+    //Home
+    "home.createArticle.button": "Create",
+    "home.emptyCreators": "There hasn't been any activity yet.",
+    "home.topCreators": "Top Creators",
     "search.filter": (params) => `Cancel search filter: \"${params[0]}\"`,
     "search.notFound": (params) =>
       `We could not find any results that match \"${params[0]}\"`,
     "order.top": "Top",
     "order.new": "New",
-    "home.topCreators": "Top Creators",
     "categories.notFound": (params) =>
       `We could not find any results for the category \"${params[0]}\"`,
     "categories.tourism": "Tourism",
@@ -20,8 +21,30 @@ const strings = {
     "categories.technology": "Technology",
     "categories.all": "All",
     "categories.forMe": "For me",
+    "views.register.title": "Register",
   },
-  "es-ES": { "home.createArticle.button": "Create cuio" },
+  "es-ES": {
+    //Home
+    "home.createArticle.button": "Crear",
+    "home.emptyCreators": "No ha habido actividad aún.",
+    "home.topCreators": "Creadores populares",
+    "search.filter": (params) => `Cancelar filtro de búsqueda: \"${params[0]}\"`,
+    "search.notFound": (params) =>
+        `No se encontraron resultados para la búsqueda \"${params[0]}\"`,
+    "order.top": "Más popular",
+    "order.new": "Nuevo",
+    "categories.notFound": (params) =>
+        `No se encontraron resultados para la categoría \"${params[0]}\"`,
+    "categories.tourism": "Turismo",
+    "categories.entertainment": "Espectáculo",
+    "categories.politics": "Política",
+    "categories.economics": "Economía",
+    "categories.sports": "Deportes",
+    "categories.technology": "Tecnología",
+    "categories.all": "Todo",
+    "categories.forMe": "Para mi",
+    "views.register.title": "Registrarse",
+  },
 };
 
 const I18n = (code, params) => {
