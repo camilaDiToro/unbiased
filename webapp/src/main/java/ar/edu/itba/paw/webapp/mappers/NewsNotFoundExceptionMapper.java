0b8c284f7bf935523ba3281bc8e70e0da978821d
camilaDiToro <camiladitoro@gmail.com>
2023-01-06 18:22:20 -0300

    Add newsNotFoundExceptionMapper

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/mappers/NewsNotFoundExceptionMapper.java
@@ -0,0 +1,18 @@
+package ar.edu.itba.paw.webapp.mappers;
+
+import ar.edu.itba.paw.model.exeptions.NewsNotFoundException;
+import ar.edu.itba.paw.webapp.dto.SimpleMessageDto;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+public class NewsNotFoundExceptionMapper implements ExceptionMapper<NewsNotFoundException> {
+
+    private static final String MESSAGE = "An article with id '%d' was not found";
+
+    @Override
+    public Response toResponse(NewsNotFoundException e) {
+        SimpleMessageDto dto = SimpleMessageDto.fromString(String.format(MESSAGE, 5));
+        return Response.status(Response.Status.NOT_FOUND).entity(dto).build();
+    }
+}
