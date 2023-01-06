ce9df2dc9687c8b11567f433acf218dfe4f44448
AleCaeiro <acaeiro@itba.edu.ar>
2023-01-06 13:52:03 -0300

    Added props to PanelCard component

--- unbiased-frontend/pages/admin/reported_news/index.js
+++ unbiased-frontend/pages/admin/reported_news/index.js
@@ -2,6 +2,7 @@ import Moderation_panel from "../../../components/Moderation_panel";
 import Tabs from "../../../components/Tabs"
 import {useAppContext} from "../../../context";
 import PanelCard from "../../../components/PanelCard";
+import Link from "next/link";
 
 export default function Reported_news(){
     const ctx = useAppContext()
@@ -12,13 +13,24 @@ export default function Reported_news(){
 
     return (
         <div className="d-flex h-100 flex-column">
-            <div className="flex-grow-1 d-flex flex-row">
+            <div className="flex-grow-1 d-flex flex-row bg-fixed">
                 <Moderation_panel/>
                 <div className="d-flex flex-column w-75 align-items-center">
                     <Tabs items={items} pill selected={ctx.I18n("reportOrder.reportCountDesc")}/>
-                    <PanelCard/>
+
+                    {
+                        [0,1,2].map((i) => {
+                            return (
+                                <PanelCard key={i} title={"TITULO"} subtitle={"Subtitulo"} timeAmount={null}
+                                           name={"Nombre"} profileImg={null} reportsCount={null}
+                                />
+                            )
+                        })
+                    }
+
                 </div>
             </div>
+            {/*TODO: if not empty newList and pagination*/}
         </div>
     )
 }
