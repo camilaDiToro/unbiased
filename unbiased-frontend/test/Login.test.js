ef804d64a5863ccbede6e0b52c26b4e8713e7cc9
AleCaeiro <acaeiro@itba.edu.ar>
2023-01-22 13:36:40 -0300

    Mocked useRouter(commited properly)

--- unbiased-frontend/test/Login.test.js
+++ unbiased-frontend/test/Login.test.js
@@ -1,10 +1,22 @@
 import {render, screen } from '@testing-library/react';
 import Login from '../pages/login/index';
 import '@testing-library/jest-dom'
+import AppWrapper from "../context";
+import {createMockRouter} from "./test_utils/createMockRouter";
+import {RouterContext} from "next/dist/shared/lib/router-context";
 
 describe('Login test', ()=>{
+
+    beforeAll(()=>{
+        render(
+            <RouterContext.Provider value={createMockRouter({})}>
+                <Login/>
+            </RouterContext.Provider>
+        );
+    })
+
     test('aparece el boton de confirmar', ()=>{
-        render(<Login/>);
+
         const button = screen.getByRole('button', {
             name: /Log in/i,
         })
