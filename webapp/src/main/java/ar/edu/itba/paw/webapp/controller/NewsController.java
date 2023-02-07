a22a27191336ee796d1ebde0a447a663ae075993
camilaDiToro <camiladitoro@gmail.com>
2023-02-07 11:06:12 -0300

    Revise endpoints

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/NewsController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/NewsController.java
@@ -292,7 +292,7 @@ public class NewsController {
 
     @Consumes({MediaType.APPLICATION_JSON})
     @POST
-    public Response createNews(@Valid final CreateNewsForm createNewsFrom){
+    public Response createNews(@Valid final CreateNewsForm createNewsFrom) {
         final User user = securityService.getCurrentUser().get();
         final News.NewsBuilder newsBuilder = new News.NewsBuilder(user, TextUtils.convertMarkdownToHTML(createNewsFrom.getBody()), createNewsFrom.getTitle(), createNewsFrom.getSubtitle());
         List<Category> categories;
@@ -304,122 +304,9 @@ public class NewsController {
         final News news = newsService.create(newsBuilder, categories);
 
         final URI location = uriInfo.getAbsolutePathBuilder().path(String.valueOf(news.getNewsId())).build();
-//        return Response.created(location).entity(NewsDto.fromNews(uriInfo, news)).build();
         return Response.created(location).build();
 
     }
-//
-//    @Consumes({MediaType.APPLICATION_JSON})
-//    @POST
-//    public Response createUser(@Valid final UserForm userForm){
-//        final User newUser = userService.create(new User.UserBuilder(userForm.getEmail()).pass(userForm.getPassword()));
-//
-//        final URI location = uriInfo.getAbsolutePathBuilder().path(String.valueOf(newUser.getId())).build();
-//        return Response.created(location).build();
-//    }
-//
-//    @GET
-//    @Path("/{userId:[0-9]+}")
-//    @Produces(value = { MediaType.APPLICATION_JSON})
-//    public Response getUser(@PathParam("userId") final long userId){
-//        User user = userService.getUserById(userId).orElseThrow(UserNotFoundException::new);
-//        UserDto userDto = UserDto.fromUser(uriInfo, user, userService.getFollowersCount(userId), userService.getFollowingCount(userId));
-//        return Response.ok(userDto).build();
-//    }
-//
-//    @GET
-//    @Path("/{userId:[0-9]+}/news-stats")
-//    @Produces(value = { MediaType.APPLICATION_JSON})
-//    public Response getUserNewsStats(@PathParam("userId") final long userId){
-//        User user = userService.getUserById(userId).orElseThrow(UserNotFoundException::new);
-//        if (!user.getRoles().contains(Role.ROLE_JOURNALIST)) {
-//            return Response.status(404).build();
-//        }
-//
-//        Map<Category, CategoryStatistics.Statistic> newsCategoryMap = newsService.getCategoryStatistics(user.getUserId()).getStatiscticsMap();
-//        List<CategoryStatisticsDto> newsStats = newsCategoryMap.entrySet().stream().map(CategoryStatisticsDto::fromCategoryStatistic).collect(Collectors.toList());
-//        final Response.ResponseBuilder responseBuilder = Response.ok(new GenericEntity<List<CategoryStatisticsDto>>(newsStats) {});
-//        return responseBuilder.build();
-//    }
-//
-//
-//    @PUT
-//    @Path("/{userId:[0-9]+}")
-//    @PreAuthorize("@ownerCheck.userMatches(#userId)")
-//    @Produces(value = { MediaType.APPLICATION_JSON})
-//    @Consumes({MediaType.APPLICATION_JSON})
-//    public Response editUser(@PathParam("userId") final long userId, @Valid final UserProfileForm userProfileForm) throws IOException {
-//        Optional<User> mayBeUser = userService.getUserById(userId);
-//
-//        if(!mayBeUser.isPresent()){
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//
-//        if(userProfileForm.getImage() != null){
-//            userService.updateProfile(userId, userProfileForm.getUsername(),
-//                    userProfileForm.getImage().getBytes(), userProfileForm.getImage().getContentType(), userProfileForm.getDescription());
-//        }else{
-//            userService.updateProfile(userId, userProfileForm.getUsername(),
-//                    null, null, userProfileForm.getDescription());
-//        }
-//        if(userProfileForm.getMailOptions()!=null){
-//            userService.updateEmailSettings(mayBeUser.get(), MailOption.getEnumCollection(userProfileForm.getMailOptions()));
-//        }
-//
-//        return Response.ok(UserDto.fromUser(uriInfo, mayBeUser.get(), userService.getFollowersCount(userId), userService.getFollowingCount(userId))).build();
-//    }
-//
-//    @GET
-//    @Path("/{userId:[0-9]+}/image")
-//    public Response profileImage(@PathParam("userId") final long userId) {
-//        final Image image = userService.getUserById(userId).orElseThrow(UserNotFoundException::new).getImage();
-//
-//        if (image.getBytes().length == 0)
-//            return Response.noContent().build();
-//
-//        return Response
-//                .ok(new ByteArrayInputStream(image.getBytes()))
-//                .type(image.getDataType())
-//                .build();
-//    }
-//
-//
-//    @PUT
-//    @PreAuthorize("@ownerCheck.newsOwnership(#newsId, #userId)")
-//    @Path(value = "/{userId:[0-9]+}/pingNews/{newsId:[0-9]+}")
-//    public Response pingNews(@PathParam("userId") final long userId, @PathParam("newsId") final long newsId) {
-//
-//        final User user = userService.getUserById(userId).orElseThrow(UserNotFoundException::new);
-//        final News news =  newsService.getById(user, newsId).orElseThrow(NewsNotFoundException::new);
-//
-//        if(userService.pingNewsToggle(user,news)){
-//            return Response.ok(SimpleMessageDto.fromString(String.format("User %s pinged the news of id %d", user.getUsername(), news.getNewsId()))).build();
-//        }else{
-//            return Response.ok(SimpleMessageDto.fromString(String.format("User %s unpinged the news of id %d", user.getUsername(), news.getNewsId()))).build();
-//        }
-//    }
-//
-//    @PUT
-//    @PreAuthorize("@ownerCheck.userMatches(#followerId)")
-//    @Path(value = "/{userId:[0-9]+}/followers/{followerId:[0-9]+}")
-//    public Response followUser(@PathParam("userId") final long userId, @PathParam("followerId") final long followerId) {
-//        final User currentUser = securityService.getCurrentUser().orElseThrow(UserNotFoundException::new);
-//        if(userService.followUser(currentUser, userId)){
-//            return Response.ok(SimpleMessageDto.fromString(String.format("User %s [id %d] followed user of id %d", currentUser, currentUser.getUserId(), userId))).build();
-//        }
-//        return Response.ok(SimpleMessageDto.fromString(String.format("User %s [id %d] already followed user of id %d", currentUser, currentUser.getUserId(), userId))).build();
-//    }
-//
-//    @DELETE
-//    @PreAuthorize("@ownerCheck.userMatches(#followerId)")
-//    @Path(value = "/{userId:[0-9]+}/followers/{followerId:[0-9]+}")
-//    public Response unfollowUser(@PathParam("userId") final long userId, @PathParam("followerId") final long followerId) {
-//        final User currentUser = securityService.getCurrentUser().orElseThrow(UserNotFoundException::new);
-//        if(userService.unfollowUser(currentUser, userId)){
-//            return Response.ok(SimpleMessageDto.fromString(String.format("User %s [id %d] unfollowed user of id %d", currentUser, currentUser.getUserId(), userId))).build();
-//        }
-//        return Response.ok(SimpleMessageDto.fromString(String.format("User %s [id %d] did not followed user of id %d", currentUser, currentUser.getUserId(), userId))).build();
-//    }
 
     @GET
     @Produces(value = {CustomMediaType.SIMPLE_MESSAGE_V1})
