369413ee95c3f72a6f183c5ba6f7c9f003e12ab7
camilaDiToro <camiladitoro@gmail.com>
2023-02-07 10:57:15 -0300

    Revise endpoints

--- unbiased-frontend/constants/index.js
+++ unbiased-frontend/constants/index.js
@@ -1,2 +1,2 @@
-export const baseURL = new URL('http://pawserver.it.itba.edu.ar/paw-2022b-6/api')
+export const baseURL = new URL('http://localhost:8080/webapp_war_exploded/api')
 //export const baseURL = new URL('http://localhost:8080/webapp_war_exploded/api/')
