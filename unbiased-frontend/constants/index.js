0495dfdbdf1a8295d7efa8e74e2019081e566156
khcatino <kcatino@itba.edu.ar>
2023-02-08 10:26:29 -0300

    Fix in URL path for api in deployment

--- unbiased-frontend/constants/index.js
+++ unbiased-frontend/constants/index.js
@@ -1,3 +1,3 @@
 const isProd = process.env.ENV === 'prod'
 
-export const baseURL = new URL(isProd ? '/paw-2022b-6' : 'http://localhost:8080/webapp_war_exploded/api')
+export const baseURL = new URL(isProd ? 'http://localhost/paw-2022b-6' : 'http://localhost:8080/webapp_war_exploded/api')
