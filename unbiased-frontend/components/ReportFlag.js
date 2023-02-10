a87c9dea21671bac59fedf5fb01724dc712c71cd
khcatino <kcatino@itba.edu.ar>
2023-02-10 16:09:02 -0300

    Add email verification to front and change paths for user images

--- unbiased-frontend/components/ReportFlag.js
+++ unbiased-frontend/components/ReportFlag.js
@@ -11,12 +11,11 @@ export default function ReportFlag(props) {
 
     let handlerArray= []
 
-    const {I18n, } = useAppContext()
+    const {I18n } = useAppContext()
     const tooltipMap = !props.comment ? {true: I18n("tooltip.articleReported"), false: I18n("tooltip.reportArticle")} :
         {true: I18n("tooltip.commentReported"), false: I18n("tooltip.reportComment")}
     return <>
         <ModalTrigger modalId={`report${props.comment ? 'Comment' : 'Article'}${props.id}`}>
-            {/*{JSON.stringify(tooltipMap)}*/}
             <Tooltip text={tooltipMap[`${!!props.reported}`]} position="bottom" >
                 <img id="save"
                      className={`icon-index ${props.reported ? '' : 'svg-btn'} svg-bookmark`}
