9e6a6b3d6bc3ee28c41a9e6596b65cd3498e8472
dlimachi <dlimachi@itba.edu.ar>
2023-02-12 15:28:45 -0300

    Added navBar test

--- unbiased-frontend/test/components/NavBar.test.js
+++ unbiased-frontend/test/components/NavBar.test.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import * as testingLibrary from "../test_utils/contextRender";
 import { getDefaultLoggedUser } from "../test_utils/defaultLoggedUser";
+import Navbar from "../../components/Navbar";
 
 jest.mock("next/link", () => {
-  return ({children}) => {
-    return children;
-  }
+  return jest.fn(() => <nav role="button">Log In</nav>)
 });
+
+jest.mock("next/link", () => {
+  return jest.fn(() => <nav role="button">Sign Up</nav>)
+});
+
 const {render, screen} = testingLibrary;
 
 let propsMap
 
 const customPropsMap = (options = {}) => {
   const map = {
-    id: 1,
-    pinned: true,
-    creatorId: 1,
+    creator: {
+      hasImage: true,
+      Image: "http://localhost:8080/webapp_war_exploded/api/users/1/image",
+      id: 1,
+      nameOrEmail: "My name",
+      tier: "default",
+    },
     triggerEffect: jest.fn()
   }
 
@@ -24,7 +32,13 @@ const customPropsMap = (options = {}) => {
 
 describe('NavBar test', ()=>{
 
-  test('NavBar show up Log In and Sign In if it is a loggedUser', ()=>{
+  beforeEach(()=>{
+    propsMap = customPropsMap()
+  })
 
+  test('NavBar show up Log In and Sign In if it is not a loggedUser', ()=>{
+    render(<Navbar  {...propsMap}/>)
+    expect(screen.getByRole('button', {name: 'Log In'})).toBeInTheDocument()
+    expect(screen.getByRole('button', {name: 'Sign Up'})).toBeInTheDocument()
   })
 })
